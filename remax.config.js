669bea121145ccd0cde9df21df05fc1846979fff
xsh <xsh@xsh.local>
2022-06-08 10:56:26 +0800

    fix: @remax/cli 编译sjs时死循环调试

--- remax.config.js
+++ remax.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   one: true,
-  output: 'dist/' + process.env.REMAX_PLATFORM
+  output: 'dist/' + process.env.REMAX_PLATFORM,
+  configWebpack({ config }) {
+    // config 是 webpack-chain 的 Config 对象
+    config.devtool('source-map');
+    debugger
+  },
 };
---
+++ src/pages/index/c.js
@@ -0,0 +1,5 @@
+debugger
+require('./a')
+// require('./b')
+
+console.log(99999)
